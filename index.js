f03474a443bad49209576297fd45715a967c1d58
Donghwan Oh <imodh97@gmail.com>
2024-04-02 10:18:01 +0900

    Use cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = rquire("cors");
 const Todo = require("./Models/Todo");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 mongoose.connect(process.env.DB_URL);
 
